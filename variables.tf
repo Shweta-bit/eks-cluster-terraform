b6ae0a6e812bcc8bdcea4720310cb295e904cf96
Shweta-bit <64265988+Shweta-bit@users.noreply.github.com>
2022-12-29 18:28:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
  // prod-products-public-subnet-1
  variable "subnet_id_1" {
   type = string
-  default = "subnet-021eae828942e3e3c"
+  default = "subnet-1"
  }
  //prod-products-eks-subnet-2
  variable "subnet_id_2" {
   type = string
-  default = "subnet-0b2e91dd2839e27cd"
+  default = "subnet-2"
  }
