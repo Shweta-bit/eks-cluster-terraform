820003aa6ed976484e2589db5bc0e4106beaa9f7
Shweta-bit <64265988+Shweta-bit@users.noreply.github.com>
2022-12-29 18:27:56 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,10 @@
+ // prod-products-public-subnet-1
+ variable "subnet_id_1" {
+  type = string
+  default = "subnet-021eae828942e3e3c"
+ }
+ //prod-products-eks-subnet-2
+ variable "subnet_id_2" {
+  type = string
+  default = "subnet-0b2e91dd2839e27cd"
+ }
